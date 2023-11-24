f037cf895f58ac460e502707a0311cad7ded7026
messaouridebounibrahim <151916278+messaouridebounibrahim@users.noreply.github.com>
2023-11-24 10:12:54 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function open_nav(){
+    document.getElementById('nav').style.display="flex";
+    document.getElementById('open_nav').style.display="none";
+    document.getElementById('close_nav').style.display="block";
+}
+function close_nav(){
+    document.getElementById('nav').style.display="none";
+    document.getElementById('open_nav').style.display="block";
+    document.getElementById('close_nav').style.display="none";
+}
